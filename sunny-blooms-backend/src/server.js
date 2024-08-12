62df7bfa58be73726c3c0fb2c3301a5cdf27b673
Alex Auch <alexsamuelauch@gmail.com>
2024-08-12 08:13:32 -0600

    fixed server for build

--- sunny-blooms-backend/src/server.js
+++ sunny-blooms-backend/src/server.js
@@ -3,11 +3,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import helmet from 'helmet'
+import path from 'path'; // Add this line
+import { fileURLToPath } from 'url';
 
 import flowerRoutes from './routes/flowers.js';
 import contactRoutes from './routes/contact.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -31,6 +36,9 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/flowers', flowerRoutes);
 app.use('/api/contact', contactRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../sunny-blooms-frontend/build/index.html'));
+});
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
